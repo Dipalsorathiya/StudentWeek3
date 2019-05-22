a174deceb95860fd8d863c3bfd9d8766e2456043
Dipalsorathiya <sorathid@sheridancollege.ca>
2019-05-22 09:37:20 -0400

    Initial Comments

---
+++ src/week3/Week3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week3;
+
+/**
+ *
+ * @author Dipal
+ */
+public class Week3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        student s1=new student();
+        s1.setFirstName("Dipal");
+        s1.setId(20);
+    }
+    
+}
---
+++ src/week3/student.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week3;
+
+/**
+ *
+ * @author Dipal
+ */
+public class student {
+    private int Id;
+    private String firstName;
+
+    /**
+     * @return the Id
+     */
+    public int getId() {
+        return Id;
+    }
+
+    /**
+     * @param Id the Id to set
+     */
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+}
